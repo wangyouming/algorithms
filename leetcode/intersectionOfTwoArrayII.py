80c463b7bf42361daa544700b27683c4aea7c49e
wangyouming <wangyouming1209@gmail.com>
2018-12-11 22:33:05 +0800

    intersectionOfTwoArrayII

---
+++ leetcode/intersectionOfTwoArrayII.py
@@ -0,0 +1,45 @@
+"""
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+URL: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+"""
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        s1 = {}
+        for ele in nums1:
+            if not ele in s1:
+                s1[ele] = 1
+            else:
+                s1[ele] += 1
+        ret = []
+        for ele in nums2:
+            if ele in s1:
+                ret.append(ele)
+                s1[ele] -= 1
+                if s1[ele] == 0:
+                    del s1[ele]
+        return ret
