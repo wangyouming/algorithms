9eccac1469c1f1b0ebf5658667fe58a8c51bcf27
wangyouming <wangyouming1209@gmail.com>
2018-12-28 00:15:06 +0800

    numMatrix

--- leetcode/numMatrix.py
+++ leetcode/numMatrix.py
@@ -32,7 +32,20 @@ class NumMatrix:
         """
         :type matrix: List[List[int]]
         """
-        
+        if not matrix: return
+        rows = len(matrix)
+        cols = len(matrix[0])
+        self.sum = [[0 for _ in range(cols)] for _ in range(rows)]
+        for row in range(rows):
+            for col in range(cols):
+                if row == 0 and col == 0:
+                    self.sum[row][col] = matrix[row][col]
+                elif row == 0:
+                    self.sum[row][col] = self.sum[row][col-1] + matrix[row][col]
+                elif col == 0:
+                    self.sum[row][col] = self.sum[row-1][col] + matrix[row][col]
+                else:
+                    self.sum[row][col] = self.sum[row-1][col] + self.sum[row][col-1] - self.sum[row-1][col-1] + matrix[row][col]
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -42,8 +55,29 @@ class NumMatrix:
         :type col2: int
         :rtype: int
         """
+        if row1 == 0 and col1 == 0:
+            return self.sum[row2][col2]
+        elif row1 == 0:
+            return self.sum[row2][col2] - self.sum[row2][col1-1]
+        elif col1 == 0:
+            return self.sum[row2][col2] - self.sum[row1-1][col2]
+        else:
+            return self.sum[row2][col2] - self.sum[row1-1][col2] - self.sum[row2][col1-1] + self.sum[row1-1][col1-1]
         
-
+if __name__ == '__main__':
+    matrix = matrix = [
+        [3, 0, 1, 4, 2],
+        [5, 6, 3, 2, 1],
+        [1, 2, 0, 1, 5],
+        [4, 1, 0, 1, 7],
+        [1, 0, 3, 0, 5]
+    ]
+    m = NumMatrix(matrix)
+    assert(m.sumRegion(2, 1, 4, 3) == 8)
+    assert(m.sumRegion(1, 1, 2, 2) == 11)
+    assert(m.sumRegion(2, 1, 4, 3) == 8)
+    assert(m.sumRegion(1, 2, 2, 4) == 12)
+    print('hello world')
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
