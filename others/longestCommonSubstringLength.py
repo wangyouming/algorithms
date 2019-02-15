6c65a8a10ba7778bf9f133fd02230ee9b4249489
wangyouming <wangyouming1209@gmail.com>
2019-02-16 00:15:20 +0800

    refactor longest common substring length

--- others/longestCommonSubstringLength.py
+++ others/longestCommonSubstringLength.py
@@ -1,57 +1,67 @@
-def longest_common_substring_length_bt(word_0: str, word_1: str) -> int:
+def lcs_bt(word_0: str, word_1: str) -> int:
     lcs = 0
-    memo = {}
-    def _longest_common_substring_length_bt(i, j, cur_lcs) -> None:
+    mem = {}
+    def _lcs_bt(i, j, cur_lcs) -> None:
         key = "{}-{}".format(i, j)
-        if key in memo and cur_lcs < memo[key]:
+        if key in mem and cur_lcs < mem[key]:
             return
         else:
-            memo[key] = cur_lcs
+            mem[key] = cur_lcs
         nonlocal lcs
         if i == len(word_0) or j == len(word_1):
             lcs = max(lcs, cur_lcs)
             return
 
         if word_0[i] == word_1[j]:
-            _longest_common_substring_length_bt(i+1, j+1, cur_lcs+1)
+            _lcs_bt(i+1, j+1, cur_lcs+1)
         else:
-            _longest_common_substring_length_bt(i+1, j, cur_lcs)
-            _longest_common_substring_length_bt(i, j+1, cur_lcs)
+            _lcs_bt(i+1, j, cur_lcs)
+            _lcs_bt(i, j+1, cur_lcs)
 
-    _longest_common_substring_length_bt(0, 0, 0)
+    _lcs_bt(0, 0, 0)
     return lcs
 
-def longest_common_substring_length_dp_fn(word_0: str, word_1: str) -> int:
-    '''
-    (i-1, j, lcs) -- +(1, 0, 0) --> (i, j, lcs)
-    (i, j-1, lcs) -- +(0, 1, 0) --> (i, j, lcs)
-    (i-1, j-1, lcs) -- +(1, 1, 1) --> (i, j, lcs+1)
+def lcs_dp_fn(word_0: str, word_1: str) -> int:
+    # if a[i] == b[j]: f(i, j) = f(i+1, j+1) + 1
+    # else: f(i, j) = max(f(i+1, j), f(i, j+1))
+    mem = {}
+    def _lcs_dp_fn(i: int, j: int) -> int:
+        key = '{}-{}'.format(i, j)
+        if key in mem: return mem[key]
+        if i >= len(word_0) or j >= len(word_1): return 0
+        res = max(_lcs_dp_fn(i+1, j), _lcs_dp_fn(i, j+1)) if word_0[i] != word_1[j] else _lcs_dp_fn(i+1, j+1) + 1
+        mem[key] = res
+        return res
+    return _lcs_dp_fn(0, 0)
 
-    if a[i] == a[j]:
-        lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1], lcs[i-1][j-1]+1)
-    else:
-        lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
-    '''
-    lcs_matrix = [[0] * len(word_1) for _ in range(len(word_0))]
-
-    for j in  range(len(word_1)):
-        if word_0[0] == word_1[j]: lcs_matrix[0][j] = 1
-        elif j > 0: lcs_matrix[0][j] = lcs_matrix[0][j-1]
+def lcs_dp_fn_matrix(word_0: str, word_1: str) -> int:
+    # if a[i] == a[j]:
+    #     f(i, j) = f(i-1, j-1) + 1
+    # else:
+    #     f(i, j) = max(f(i-1, j), f(i, j-1))
+    matrix = [[0] * len(word_1) for _ in range(len(word_0))]
 
     for i in range(len(word_0)):
-        if word_0[i] == word_1[0]: lcs_matrix[i][0] = 1
-        elif i > 0: lcs_matrix[i][0] = lcs_matrix[i-1][0]
+        if word_0[i] == word_1[0]: matrix[i][0] = 1
+        elif i > 0: matrix[i][0] = matrix[i-1][0]
+        else: matrix[i][0] = 0
+    
+    for j in range(len(word_1)):
+        if word_0[0] == word_1[j]: matrix[0][j] == 1
+        elif j > 0: matrix[0][j] = matrix[0][j-1]
+        else: matrix[0][j] = 0
 
     for i in range(1, len(word_0)):
         for j in range(1, len(word_1)):
             if word_0[i] == word_1[j]:
-                lcs_matrix[i][j] = max(lcs_matrix[i-1][j], lcs_matrix[i][j-1], lcs_matrix[i-1][j-1]+1)
+                matrix[i][j] = matrix[i-1][j-1] + 1
             else:
-                lcs_matrix[i][j] = max(lcs_matrix[i-1][j], lcs_matrix[i][j-1])
-    return lcs_matrix[-1][-1]
+                matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
+    return matrix[-1][-1]
 
 if __name__ == '__main__':
-    word_0 = "helloworld"
-    word_1 = "takemefaraway"
-    print(longest_common_substring_length_bt(word_0, word_1))
-    print(longest_common_substring_length_dp_fn(word_0, word_1))
+    word_0 = "hello world"
+    word_1 = "take me far away"
+    print(lcs_bt(word_0, word_1))
+    print(lcs_dp_fn(word_0, word_1))
+    print(lcs_dp_fn_matrix(word_0, word_1))
