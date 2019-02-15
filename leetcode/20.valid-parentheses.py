b86af3a793205852f292e4b045fe96c87b94e353
wangyouming <wangyouming1209@gmail.com>
2019-02-16 03:18:15 +0800

    20.valid-parentheses.py

---
+++ leetcode/20.valid-parentheses.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# https://leetcode.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (35.71%)
+# Total Accepted:    505.5K
+# Total Submissions: 1.4M
+# Testcase Example:  '"()"'
+#
+# Given a string containing just the characters '(', ')', '{', '}', '[' and
+# ']', determine if the input string is valid.
+# 
+# An input string is valid if:
+# 
+# 
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# 
+# 
+# Note that an empty string is also considered valid.
+# 
+# Example 1:
+# 
+# 
+# Input: "()"
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: "()[]{}"
+# Output: true
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: "(]"
+# Output: false
+# 
+# 
+# Example 4:
+# 
+# 
+# Input: "([)]"
+# Output: false
+# 
+# 
+# Example 5:
+# 
+# 
+# Input: "{[]}"
+# Output: true
+# 
+# 
+#
+class Solution:
+    def isValid(self, s: 'str') -> 'bool':
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+        for char in s:
+            if char not in pairs:
+                stack.append(char)
+            else:
+                if stack and stack[-1] == pairs[char]:
+                    stack.pop()
+                else:
+                    stack.append(char)
+        return not stack
