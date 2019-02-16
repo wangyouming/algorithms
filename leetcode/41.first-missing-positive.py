4d7e4a7967de6052cc328f6e2738a8a7830a6751
wangyouming <wangyouming1209@gmail.com>
2019-02-17 01:22:05 +0800

    41.first-missing-positive

---
+++ leetcode/41.first-missing-positive.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=41 lang=python
+#
+# [41] First Missing Positive
+#
+# https://leetcode.com/problems/first-missing-positive/description/
+#
+# algorithms
+# Hard (28.04%)
+# Total Accepted:    188K
+# Total Submissions: 669.6K
+# Testcase Example:  '[1,2,0]'
+#
+# Given an unsorted integer array, find the smallest missing positive integer.
+# 
+# Example 1:
+# 
+# 
+# Input: [1,2,0]
+# Output: 3
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [3,4,-1,1]
+# Output: 2
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: [7,8,9,11,12]
+# Output: 1
+# 
+# 
+# Note:
+# 
+# Your algorithm should run in O(n) time and uses constant extra space.
+# 
+#
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            while 0 <= nums[i]-1 < len(nums) and nums[nums[i]-1] != nums[i]:
+                idx = nums[i] - 1
+                nums[i], nums[idx] = nums[idx], nums[i]
+        for idx, num in enumerate(nums):
+            if idx != num-1: return idx+1
+        return len(nums)+1
+                    
