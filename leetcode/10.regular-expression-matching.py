ce05f72892007a669cd3845e36f51f03ff4dfee5
wangyouming <wangyouming1209@gmail.com>
2019-02-21 16:36:42 +0800

    10.regular-expression-matching

---
+++ leetcode/10.regular-expression-matching.py
@@ -0,0 +1,129 @@
+#
+# @lc app=leetcode id=10 lang=python
+#
+# [10] Regular Expression Matching
+#
+# https://leetcode.com/problems/regular-expression-matching/description/
+#
+# algorithms
+# Hard (24.85%)
+# Total Accepted:    274K
+# Total Submissions: 1.1M
+# Testcase Example:  '"aa"\n"a"'
+#
+# Given an input string (s) and a pattern (p), implement regular expression
+# matching with support for '.' and '*'.
+# 
+# 
+# '.' Matches any single character.
+# '*' Matches zero or more of the preceding element.
+# 
+# 
+# The matching should cover the entire input string (not partial).
+# 
+# Note:
+# 
+# 
+# s could be empty and contains only lowercase letters a-z.
+# p could be empty and contains only lowercase letters a-z, and characters like
+# . or *.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input:
+# s = "aa"
+# p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# s = "aa"
+# p = "a*"
+# Output: true
+# Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore,
+# by repeating 'a' once, it becomes "aa".
+# 
+# 
+# Example 3:
+# 
+# 
+# Input:
+# s = "ab"
+# p = ".*"
+# Output: true
+# Explanation: ".*" means "zero or more (*) of any character (.)".
+# 
+# 
+# Example 4:
+# 
+# 
+# Input:
+# s = "aab"
+# p = "c*a*b"
+# Output: true
+# Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore
+# it matches "aab".
+# 
+# 
+# Example 5:
+# 
+# 
+# Input:
+# s = "mississippi"
+# p = "mis*is*p*."
+# Output: false
+# 
+# 
+#
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        global match
+        match = False
+        def backtrace(s_idx, p_idx):
+            global match
+            if match: return
+
+            if p_idx == len(p):
+                if s_idx == len(s):
+                    match = True
+                return
+
+            if p_idx < len(p)-1 and p[p_idx+1] == '*':
+                if s_idx == len(s):
+                    backtrace(s_idx, p_idx+2)
+                else:
+                    if p[p_idx] == '.':
+                        for i in range(s_idx, len(s)+1):
+                            backtrace(i, p_idx+2) 
+                    else:
+                        cnt = 0
+                        while s_idx+cnt < len(s) and s[s_idx+cnt] == p[p_idx]:
+                            cnt += 1
+                        for i in range(cnt+1):
+                            backtrace(s_idx+i, p_idx+2)
+            elif s_idx < len(s):
+                if p[p_idx] == '.' or s[s_idx] == p[p_idx]:
+                    backtrace(s_idx+1, p_idx+1)
+                
+        backtrace(0, 0)
+        return match 
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isMatch("aa", "a"))
+    print(solution.isMatch("aa", "a*"))
+    print(solution.isMatch("ab", ".*"))
+    print(solution.isMatch("aab", "c*a*b"))
+    print(solution.isMatch("mississippi", "mis*is*p*"))
+        
