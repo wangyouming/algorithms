630cec79c788b0c98b26f039342cdf652228dd83
wangyouming <wangyouming1209@gmail.com>
2019-02-23 13:34:10 +0800

    string bf rk

--- others/bf_rk.py
+++ others/bf_rk.py
@@ -18,10 +18,10 @@ def bf(text, pattern):
                 break
     return -1
 
-def simple_hash(s,start, end):
-    assert(start <= end)
+def simple_hash(s, start, end):
+    assert(start < end)
     res = 0
-    for c in s[start:end+1]:
+    for c in s[start:end]:
         res += ord(c)
     return res
 
@@ -33,14 +33,14 @@ def rk(text, pattern):
         return 0 if pattern == text else -1
 
     hash_memo = [None] * (n-m+1)
-    hash_memo[0] = simple_hash(text, 0, m-1)
+    hash_memo[0] = simple_hash(text, 0, m)
     for i in range(1, n-m+1):
-        hash_memo[i] = hash_memo[i-1] - simple_hash(text, i-1, i-1) + simple_hash(text, i+m-1, i+m-1)
+        hash_memo[i] = hash_memo[i-1] - simple_hash(text, i-1, i) + simple_hash(text, i+m-1, i+m)
     
-    hash_p = simple_hash(pattern, 0, m-1)
+    pattern_hash = simple_hash(pattern, 0, m)
 
     for i, h in enumerate(hash_memo):
-        if h == hash_p:
+        if h == pattern_hash:
             if pattern == text[i:i+m]:
                 return i
             else:
@@ -48,21 +48,21 @@ def rk(text, pattern):
     return -1
 
 if __name__ == '__main__':
-    m_str = 'a'*10000
-    p_str = 'a'*200+'b'
+    text = 'a'*10000
+    pattern = 'a'*200+'b'
 
     print('--- time consume ---')
     t = time()
-    print('[bf] result:', bf(m_str, p_str))
+    print('[bf] result:', bf(text, pattern))
     print('[bf] time cost: {0:.5}s'.format(time()-t))
 
     t = time()
-    print('[rk] result:', rk(m_str, p_str))
+    print('[rk] result:', rk(text, pattern))
     print('[rk] time cost: {0:.5}s'.format(time()-t))
 
     print('')
     print('--- search ---')
-    m_str = 'thequickbrownfoxjumpsoverthelazydog'
-    p_str = 'jump'
-    print('[bf] result:', bf(m_str, p_str))
-    print('[rk] result:', rk(m_str, p_str))
+    text = 'the quick brown fox jumps over the lazy dog'
+    pattern = 'jump'
+    print('[bf] result:', bf(text, pattern))
+    print('[rk] result:', rk(text, pattern))
