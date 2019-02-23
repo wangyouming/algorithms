37e15ed0d674b75b2c2ef20cb800ce4dd8e6b6a6
wangyouming <wangyouming1209@gmail.com>
2019-02-23 15:14:18 +0800

    kmp

--- others/kmp.py
+++ others/kmp.py
@@ -1,30 +1,32 @@
-class KMP:
-    def getNexts(self, pattern: str):
-        self.next = [-1] * len(pattern)
-        k = -1
-        i = 1
-        while i < len(pattern):
-            while k != -1 and pattern[k + 1] != pattern[i]:
-                k = self.next[k]
-            if pattern[k + 1] == pattern[i]:
-                k += 1
-            self.next[i] = k
-            i += 1
+from typing import List
+
+def getNext(pattern: str) -> List[int]:
+    next = [-1] * len(pattern)
+    k = -1
+    i = 1
+    while i < len(pattern):
+        while k != -1 and pattern[k + 1] != pattern[i]:
+            k = next[k]
+        if pattern[k + 1] == pattern[i]:
+            k += 1
+        next[i] = k
+        i += 1
+    return next
         
-    def index(self, candidate: str, pattern: str):
-        self.getNexts(pattern)
-        i = 0
-        j = 0
-        while i < len(candidate):
-            while j > 0 and candidate[i] != pattern[j]:
-                j = self.next[j - 1] + 1
-            if candidate[i] == pattern[j]:
-                j += 1
-            if j == len(pattern):
-                return i - len(pattern) + 1
-            i += 1
-        return -1
+def kmp(text: str, pattern: str):
+    next = getNext(pattern)
+    i = 0
+    j = 0
+    while i < len(text):
+        while j > 0 and text[i] != pattern[j]:
+            j = next[j - 1] + 1
+        if text[i] == pattern[j]:
+            j += 1
+        if j == len(pattern):
+            return i - len(pattern) + 1
+        i += 1
+    return -1
 
 if __name__ == '__main__':
-    assert(KMP().index('hello world', 'world') == 'hello world'.index('world'))
+    assert(kmp('hello world', 'world') == 'hello world'.index('world'))
         
