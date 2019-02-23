a57340a0f94c6071896bd63c991d49f1da3dee71
wangyouming <wangyouming1209@gmail.com>
2019-02-23 14:30:23 +0800

    bm

--- others/bm.py
+++ others/bm.py
@@ -1,49 +1,50 @@
-class BM:
-    def generateBadCharacter(self, pattern, badCharacter):
-        for idx, character in enumerate(pattern):
-            ascii = ord(character)
-            badCharacter[ascii] = idx
+from typing import List, Tuple
 
-    def generateGoodSuffix(self, pattern, suffix, prefix):
-        for i in range(len(pattern)-1):
-            j = i
-            k = 0
-            while (j >= 0 and pattern[j] == pattern[len(pattern) - 1 - k]):
-                j -= 1
-                k += 1
-                suffix[k] = j + 1
-            if j == -1: prefix[k] = True
-    
-    def moveByGoodSuffix(self, j, m, suffix, prefix):
-        k = m - 1 - j
-        if suffix[k] != -1: return j + 1 - suffix[k]
-        for r in range(j+2, m):
-            if prefix[m-r] == True:
-                return r
-        return m
+def generateBC(pattern: str) -> List[int]:
+    bc = [-1 for _ in range(256)]
+    for idx, char in enumerate(pattern):
+        ascii = ord(char)
+        bc[ascii] = idx
+    return bc
 
-    def indexOf(self, candidate, pattern):
-        badCharacter = [-1 for _ in range(256)]
-        self.generateBadCharacter(pattern, badCharacter)
-        suffix = [-1 for _ in range(len(pattern))]
-        prefix = [False for _ in range(len(pattern))]
-        self.generateGoodSuffix(pattern, suffix, prefix)
-        i = 0
-        while i <= len(candidate) - len(pattern):
-            j = len(pattern) - 1
-            while j >= 0:
-                if candidate[i+j] != pattern[j]: break
-                j -= 1
-            if j < 0: return i
-            x = j - badCharacter[ord(candidate[j+i])]
-            y = 0
-            if j < len(pattern) - 1:
-                y = self.moveByGoodSuffix(j, len(pattern), suffix, prefix)
-            i = i + max(x, y)
+def generateGS(pattern: str) -> Tuple[List[int], List[int]]:
+    suffix = [-1 for _ in range(len(pattern))]
+    prefix = [False for _ in range(len(pattern))]
+    for i in range(len(pattern)-1):
+        j = i
+        k = 0
+        while (j >= 0 and pattern[j] == pattern[len(pattern) - 1 - k]):
+            suffix[k] = j
+            j -= 1
+            k += 1
+        if j == -1: prefix[k] = True
+    return suffix, prefix
 
-        return -1
+def moveByGS(bad_idx: int, pattern_len: int, suffix: List[int], prefix: List[int]) -> int:
+    k = pattern_len - 1 - bad_idx
+    if suffix[k] != -1: return bad_idx + 1 - suffix[k]
+    for begin in range(bad_idx+2, pattern_len):
+        if prefix[pattern_len-begin] == True:
+            return begin
+    return pattern_len
+
+def bm(text: str, pattern: str) -> int:
+    bc = generateBC(pattern)
+    suffix, prefix = generateGS(pattern)
+    i = 0
+    while i <= len(text) - len(pattern):
+        j = len(pattern) - 1
+        while j >= 0:
+            if text[i+j] != pattern[j]: break
+            j -= 1
+        if j < 0: return i
+        x = j - bc[ord(text[i+j])]
+        y = 0
+        if j < len(pattern) - 1:
+            y = moveByGS(j, len(pattern), suffix, prefix)
+        i = i + max(x, y)
+
+    return -1
 
 if __name__ == '__main__':
-    bm = BM()
-    idx = bm.indexOf('somehellosome', 'hello')
-    print(idx)
+    print(bm('hello world', 'world'))
