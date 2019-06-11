b4d6b7a1fa5fed5a613909f83e88a3d707b80949
wangyouming <wangyouming1209@gmail.com>
2019-06-11 18:44:17 +0800

    4.median-of-two-sorted-arrays

---
+++ leetcode/4.median-of-two-sorted-arrays.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode id=4 lang=python3
+#
+# [4] Median of Two Sorted Arrays
+#
+# https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+#
+# algorithms
+# Hard (26.40%)
+# Likes:    4338
+# Dislikes: 581
+# Total Accepted:    439.1K
+# Total Submissions: 1.7M
+# Testcase Example:  '[1,3]\n[2]'
+#
+# There are two sorted arrays nums1 and nums2 of size m and n respectively.
+# 
+# Find the median of the two sorted arrays. The overall run time complexity
+# should be O(log (m+n)).
+# 
+# You may assume nums1 and nums2 cannot be both empty.
+# 
+# Example 1:
+# 
+# 
+# nums1 = [1, 3]
+# nums2 = [2]
+# 
+# The median is 2.0
+# 
+# 
+# Example 2:
+# 
+# 
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+# 
+# The median is (2 + 3)/2 = 2.5
+# 
+# 
+#
+
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            m, n, nums1, nums2 = n, m, nums2, nums1
+        if n == 0: return 0
+        
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and nums2[j-1] > nums1[i]:
+                imin = i + 1
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                imax = i - 1
+            else:
+                if i == 0: max_of_left = nums2[j-1]
+                elif j == 0: max_of_left = nums1[i-1]
+                else: max_of_left = max(nums1[i-1], nums2[j-1])
+                
+                if (m + n) % 2 == 1:
+                    return float(max_of_left)
+                
+                if i == m: min_of_right = nums2[j]
+                elif j == n: min_of_right = nums1[i]
+                else: min_of_right = min(nums1[i], nums2[j])
+                
+                return (max_of_left + min_of_right) / 2
+
