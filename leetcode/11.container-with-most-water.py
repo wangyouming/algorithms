7314ae0f9a832bce7a0413c397980c2a07c1e92d
wangyouming <wangyouming1209@gmail.com>
2019-06-13 16:07:58 +0800

    11.container-with-most-water

---
+++ leetcode/11.container-with-most-water.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=11 lang=python3
+#
+# [11] Container With Most Water
+#
+# https://leetcode.com/problems/container-with-most-water/description/
+#
+# algorithms
+# Medium (44.65%)
+# Likes:    3334
+# Dislikes: 444
+# Total Accepted:    380.6K
+# Total Submissions: 849K
+# Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+#
+# Given n non-negative integers a1, a2, ..., an , where each represents a point
+# at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
+# of line i is at (i, ai) and (i, 0). Find two lines, which together with
+# x-axis forms a container, such that the container contains the most water.
+# 
+# Note: You may not slant the container and n is at least 2.
+# 
+# 
+# 
+# 
+# 
+# The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+# this case, the max area of water (blue section) the container can contain is
+# 49. 
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input: [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# 
+#
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right, width, res = 0, len(height)-1, len(height)-1, 0
+        for w in range(width, 0, -1):
+            if height[left] < height[right]:
+                res, left = max(height[left]*w, res), left + 1
+            else:
+                res, right = max(height[right]*w, res), right - 1
+        return res
