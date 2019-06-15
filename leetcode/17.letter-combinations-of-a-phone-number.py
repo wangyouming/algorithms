a732a90154c968c331bb6c3ebf1e576011af266d
wangyouming <wangyouming1209@gmail.com>
2019-06-15 16:47:40 +0800

    17.letter-combinations-of-a-phone-number

---
+++ leetcode/17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+#
+# algorithms
+# Medium (41.77%)
+# Likes:    2218
+# Dislikes: 298
+# Total Accepted:    397K
+# Total Submissions: 950.4K
+# Testcase Example:  '"23"'
+#
+# Given a string containing digits from 2-9 inclusive, return all possible
+# letter combinations that the number could represent.
+# 
+# A mapping of digit to letters (just like on the telephone buttons) is given
+# below. Note that 1 does not map to any letters.
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input: "23"
+# Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+# 
+# 
+# Note:
+# 
+# Although the above answer is in lexicographical order, your answer could be
+# in any order you want.
+# 
+#
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        m = {'2': 'abc',
+             '3': 'def',
+             '4': 'ghi',
+             '5': 'jkl',
+             '6': 'mno',
+             '7': 'pqrs',
+             '8': 'tuv',
+             '9': 'wxyz'}
+        def backtrace(combination, next_digits):
+            if len(next_digits) == 0:
+                output.append(combination)
+            else:
+                for letter in m[next_digits[0]]:
+                    backtrace(combination + letter, next_digits[1:])
+        output = []
+        if digits:
+            backtrace("", digits)
+        return output
