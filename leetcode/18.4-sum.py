af39ccb4894c8562ad4099a1c7076618a89e693f
wangyouming <wangyouming1209@gmail.com>
2019-06-16 13:53:28 +0800

    18.4-sum

--- leetcode/18.4-sum.py
+++ leetcode/18.4-sum.py
@@ -40,6 +40,34 @@ from typing import List
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        return self.fourSum_1(nums, target)
+    
+    def fourSum_1(self, nums: List[int], target: int) -> List[List[int]]:
+        def nSum(l: int, r: int, target: int, N: int, result: List[int], results: List[List[int]]):
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:
+                return
+            if N == 2:
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:
+                for i in range(l, r + 1):
+                    if i == l or (i > l and nums[i-1] != nums[i]):
+                        nSum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
+        nums.sort()
+        results = []
+        nSum(0, len(nums)-1, target, 4, [], results)
+        return results
+    
+    def fourSum_0(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         res = []
         if len(nums) < 4: return res
